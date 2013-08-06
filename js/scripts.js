eefa36552e3e1bd11c92f82d44f0cc58c913d1c3
nlb-fueled <nlb@fueled.co>
2013-08-06 15:12:12 +0530

    Display All and Search working

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,20 @@
-function verifyPerson(){
-    if($("#name").val()=="" || $("#address").val()==""){
-        alert("Incomplete form");
-        return;
-    }
-    else{
-        html5rocks.webdb.addPerson= function(todoText){
-            var db= html5rocks.webdb.db;
-            db.transaction(function(tx){
-                tx.executeSql("INSERT INTO Person(Name, Address) Values (?,?)",[$("name").val(), $("address").val()], html5rocks.webdb.onSuccess, html5rocks.webdb.onError);} );}
 
-
-    }
-}
-function verifySearch(cmd){
-    if(cmd=='search'){
-        if($("#search").val()==""){
-            alert("incomplete form");
-            return;
-        }
-    }
-   else if(cmd=='delete'){
-        if($("#delete").val()==""){
-            alert("incomplete form");
-            return;
-        }
-    }
-
-        alert("form accepted");
-
-}
-
-function sortform(){
-
-    alert("yoyo");
-}
 var html5rocks = {};
 html5rocks.webdb = {};
 html5rocks.webdb.db = null;
 
 html5rocks.webdb.open = function() {
   var dbSize = 5 * 1024 * 1024; // 5MB
-  html5rocks.webdb.db = openDatabase("Todo", "1.0", "Todo manager", dbSize);
+  html5rocks.webdb.db = openDatabase("Person", "1.0", "Person manager", dbSize);
 }
 
 html5rocks.webdb.onError = function(tx, e) {
-  alert("There has been an error: " + e.message);
+ alert("There has been an error: " + e.message);
 }
 
 html5rocks.webdb.onSuccess = function(tx, r) {
   // re-render the data.
-  // loadTodoItems is defined in Step 4a
-  html5rocks.webdb.getAllTodoItems(loadTodoItems);
+  loadTodoItems(tx, r);
 }
 html5rocks.webdb.createTable = function() {
   var db = html5rocks.webdb.db;
@@ -65,4 +29,92 @@ html5rocks.webdb.getAllPerson = function(renderFunc) {
     tx.executeSql("SELECT * FROM Person", [], renderFunc,
         html5rocks.webdb.onError);
   });
+
+}
+html5rocks.webdb.addPerson= function(personName, address){
+    var db= html5rocks.webdb.db;
+    db.transaction(function(tx){
+        tx.executeSql("INSERT INTO Person(Name, Address) Values (?,?)",[personName, address], html5rocks.webdb.onSuccess, html5rocks.webdb.onError);} );}
+function init(){
+  html5rocks.webdb.open();
+  html5rocks.webdb.createTable();
+  html5rocks.webdb.getAllPerson(loadTodoItems);
+}
+function addPerson(){
+  alert($("#name").val());
+  html5rocks.webdb.addPerson($("#name").val(), $("#address").val());
+  $("#name").val()=""; $("#.address").val()="";
+}
+function renderTodo(row) {
+  return "<tr><td>" + row.Name +"</td><td>"+ row.Address+"</td></tr>";
+}
+function loadTodoItems(tx, rs) {
+        var rowOutput = "";
+        var todoItems = document.getElementById("personList");
+        for (var i=0; i < rs.rows.length; i++) {
+          rowOutput += renderTodo(rs.rows.item(i));
+        }
+
+        todoItems.innerHTML = rowOutput;
+  }
+  function loadSearchItems(tx, rs) {
+
+          alert("loading search");
+          var rowOutput = "";
+          var todoItems = document.getElementById("searchList");
+
+          for (var i=0; i < rs.rows.length; i++) {
+            rowOutput += renderTodo(rs.rows.item(i));
+          }
+
+          todoItems.innerHTML = rowOutput;
+          return;
+    }
+
+
+
+function search(renderFunc){
+  var name= $("#search").val();
+  var db= html5rocks.webdb.db;
+  db.transaction(function(tx){
+      tx.executeSql("Select * from Person where Name=(?)",[name], renderFunc, html5rocks.webdb.onError);
+  });
+
+ }
+
+
+
+
+
+
+
+
+function verifyPerson(){
+    if($("#name").val()=="" || $("#address").val()==""){
+        alert("Incomplete form");
+        return;
+    }
+    else{
+        addPerson();
+    }
+}
+function verifySearch(cmd){
+    if(cmd=='search'){
+        if($("#search").val()==""){
+            alert("incomplete form");
+            return;
+        }
+        search(loadSearchItems);
+    }
+   else if(cmd=='delete'){
+        if($("#delete").val()==""){
+            alert("incomplete form");
+            return;
+        }
+    }
+}
+
+function sortform(){
+
+    alert("abc d");
 }
